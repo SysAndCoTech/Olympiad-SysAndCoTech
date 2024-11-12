b972310cefa1770d9a0afdf84e11ebac9202b521
kamaterasu <togoldor.bayrtsolmon@gmail.com>
2024-11-12 15:18:20 +0800

    medal

--- src/component/Reward/reward.js
+++ src/component/Reward/reward.js
@@ -10,18 +10,18 @@ const Reward = () => {
           <div className="reward-text">
             <div className="reward-text-st">Тэргүүн байр</div>
             <h3 className="reward-text-bg">400'000₮</h3>
-            <h5>Медал, Өргөмжлөл, Мөнгөн шагнал</h5>
+            <h5>Медаль, Өргөмжлөл, Мөнгөн шагнал</h5>
           </div>
           <div className="reward-text2">
             <div className="reward-text">
               <div className="reward-text-st">Дэд байр</div>
               <h3 className="reward-text-bg">300'000₮</h3>
-              <h5>Медал, Өргөмжлөл, Мөнгөн шагнал</h5>
+              <h5>Медаль, Өргөмжлөл, Мөнгөн шагнал</h5>
             </div>
             <div className="reward-text">
               <div className="reward-text-st">Гутгаар байр</div>
               <h3 className="reward-text-bg">200'000₮</h3>
-              <h5>Медал, Өргөмжлөл, Мөнгөн шагнал</h5>
+              <h5>Медаль, Өргөмжлөл, Мөнгөн шагнал</h5>
             </div>
           </div>
         </div>
